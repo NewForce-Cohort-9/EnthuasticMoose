171da22983c1c2cd647a9b78ccbaa899b415f2ea
Sarah Brooks <sarahebrooks12@gmail.com>
2024-07-16 10:33:00 -0400

    phase 6 complete

--- Program.cs
+++ Program.cs
@@ -9,7 +9,15 @@ MooseSays("I really am enthusiastic");
 
 // As a question
 bool isTrue = MooseAsks("Is Canada real?");
-Console.WriteLine(isTrue);
+// Console.WriteLine(isTrue); <-- testing if we get true or false response back
+if (isTrue)
+{
+    MooseSays("Really? It seems very unlikely.");
+}
+else
+{
+    MooseSays("I  K N E W  I T !!!");
+}
 
 //Function - JavaScript
 // function MooseSays() {
