67a3632f408a0bd886591d1d687c2d7e76ed527d
Sarah Brooks <sarahebrooks12@gmail.com>
2024-07-16 10:20:47 -0400

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+﻿using System;
+
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+MooseSays();
+
+//Function - JavaScript
+// function MooseSays() {
+//     return console.log()
+// }
+
+//Fat Arrow Function - JavaScript
+// const MooseSays = () => {
+//     return console.log()
+// }
+
+// void is a function without returning anything - an action like a verb
+void MooseSays()
+{
+    Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
